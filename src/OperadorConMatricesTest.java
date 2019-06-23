fc0a9b3aa2d661065d74a20842a2c3ecfc812ff0
Alejandro <alefacuferrero@gmail.com>
2019-06-23 12:38:56 -0300

    hecho el test operador con matrices

--- src/OperadorConMatricesTest.java
+++ src/OperadorConMatricesTest.java
@@ -1,34 +1,61 @@
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class OperadorConMatricesTest {
+    int[][] matA = {{1,2,3},
+                    {3,2,1},
+            {1,1,1},
+            {0,0,0},
+            {0,1,0}
+    };
+    int[][] matB = {{1,2,3,4,5},
+                    {0,0,0,0,0},
+                    {1,1,1,-1,-1}};
+    int[] vectA = {1, 2, 3, 1, 2, 3};
+    int[] vectB = {2, 1, 2, 1, 2, 1};
+    int[] v1 = {1,0,1,0};
+    int[] v2 = {1,1,0,0};
+    int[][] m1 = {{1,1,1,1},{0,1,0,1},{0,0,0,0}};
+    int[][] m2 = {{0,1,1,0},{0,0,1,1},{1,1,0,1}};
+    private OperadorConMatrices op;
 
-    @org.junit.Before
+    @Before
     public void setUp() throws Exception {
-        int[][] matA = {{1,2,3},
-                        {3,2,1},
-                        {1,1,1},
-                        {0,0,0},
-                        {0,1,0}
-                                };
-        int[][] matB = {{1,2,3,4,5},
-                        {0,0,0,0,0},
-                        {1,1,1,-1,-1},
-                                    };
+       op = new OperadorConMatrices();
 }
 
-    @org.junit.Test
-    public void and() {
+    @Test
+    public void andVectores() {
+        int[] esperado = {1,0,0,0};
+        assertArrayEquals(esperado,op.and(v1,v2));
     }
 
-    @org.junit.Test
-    public void and1() {
+    @Test
+    public void andMatrices() {
+        int[][] esperado = {{0,1,1,0},{0,0,0,1},{0,0,0,0}};
+        assertArrayEquals(esperado,op.and(m1,m2));
     }
 
-    @org.junit.Test
-    public void multiplicar() {
+    @Test
+    public void multiplicarVectores() {
+        int esperado = 18;
+        assertEquals(esperado,op.multiplicar(vectA,vectB));
     }
 
-    @org.junit.Test
-    public void multiplicar1() {
+    @Test
+    public void multiplicarMatrices() {
+        int[][] esperado = {{4,5,6,1,2},{4,7,10,11,14},{2,3,4,3,4},{0,0,0,0,0},{0,0,0,0,0}};
+        assertArrayEquals(esperado,op.multiplicar(matA,matB));
+
+    }
+
+    @Test
+    public void sumarVectores(){
+        int[] esperado = {3,3,5,2,4,4};
+        assertArrayEquals(esperado,op.sumar(vectA,vectB));
+
+
     }
 }
